4c75d0184d7c2d807b8989005a03dd544387a38d
hemesath <hemesath@neuro.mpg.de>
2021-01-27 11:55:50 +0100

    Add to the plan

--- main.py
+++ main.py
@@ -2,5 +2,10 @@
 Input: str - DNA sequence
 Load JSONs
 Create lookup dictionary to make the antisense strand
+Make antisense strand
+flip antisense strand
+Convert to RNA (both strands)
+Loop through the strands, read codons, save index of AUG
+
 
 '''
